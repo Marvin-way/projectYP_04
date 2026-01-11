192a593059ccf8c27936a6654f914c85e3e1d733
Marvin <danis210594@gmail.com>
2026-01-11 22:47:14 +0300

    Sprint 05: start

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Данис Байрамгулов on 11.01.2026.
+//
+
+import Foundation
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Данис Байрамгулов on 11.01.2026.
+//
+import UIKit
+import Foundation
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
